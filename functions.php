a197dc3e15a224acf4d4cb7bf724fa0129bb1619
Dave Kellam <dave@eightface.com>
2013-12-05 03:09:59 -0500

    Bringing a bunch of things up to WP formatting standards

--- functions.php
+++ functions.php
@@ -9,11 +9,11 @@ if ( ! isset( $content_width ) )
 // remove cruft from the WordPress header
 remove_action( 'wp_head', 'wlwmanifest_link' );
 remove_action( 'wp_head', 'rsd_link' );
-remove_action( 'wp_head', 'index_rel_link');
-remove_action( 'wp_head', 'parent_post_rel_link');
-remove_action( 'wp_head', 'start_post_rel_link'); 
-remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head'); 
-remove_action( 'wp_head', 'wp_generator');
+remove_action( 'wp_head', 'index_rel_link' );
+remove_action( 'wp_head', 'parent_post_rel_link' );
+remove_action( 'wp_head', 'start_post_rel_link' ); 
+remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head' ); 
+remove_action( 'wp_head', 'wp_generator' );
 
 // remove Jetpack cruft
 function remove_jetpack_styles() {
--- home.php
+++ home.php
@@ -1,21 +1,23 @@
 <?php get_header(); ?>
 
 <section id="primary">	
-	<?php while (have_posts()) : the_post(); ?>	
- 	<article>
-  		<h2><a href="<?php the_permalink() ?>" title="<?php the_title(); ?> &mdash; <?php the_time('F j, Y') ?>"><?php the_title(); ?></a></h2> 
-  		<?php the_content(); ?>	
-  		<aside class="meta">
-   			<?php the_time('F jS') ?>
-  			<?php if(get_the_tags()) : ?>&middot; <?php the_tags('', ' '); ?><?php endif; ?>
-			<?php edit_post_link("edit"," &middot; "); ?> 
-		</aside>
- 	</article>
+	<?php while ( have_posts() ) : the_post(); ?>	
+		<article>
+			<h1><a href="<?php the_permalink() ?>" title="<?php the_title(); ?> &mdash; <?php the_time('F j, Y') ?>"><?php the_title(); ?></a></h1> 
+			
+			<?php the_content(); ?>	
+			
+			<aside class="meta">
+				<?php the_time('F jS') ?>
+				<?php if( get_the_tags() ) : ?>&middot; <?php the_tags( '', ' ' ); ?><?php endif; ?>
+			</aside>
+		</article>
+ 	
  	<?php endwhile; ?>
 
 	<div id="pagination">
-			<?php next_posts_link('&larr; Earlier'); ?>
-			<?php previous_posts_link('Later &rarr;'); ?>
+			<?php next_posts_link( '&larr; Earlier' ); ?>
+			<?php previous_posts_link( 'Later &rarr;' ); ?>
 	</div>
 </section>
 
