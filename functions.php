4b0a5aef6bff1611b4991555973d5e5d0a76a0d4
Dave Kellam <dave@eightface.com>
2013-12-16 01:29:46 -0500

    Add html5 shiv to support earlier web browsers

--- functions.php
+++ functions.php
@@ -80,3 +80,13 @@ function ef_wp_title( $title, $sep ) {
 	return $title;
 }
 // add_filter( 'wp_title', 'ef_wp_title', 10, 2 );
+
+/**
+ * Add html5.js script to <head> conditionally for IE8 and under
+ */
+function ef_ie_html5_js() { ?>
+        <!--[if lt IE 9]>
+        	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+        <![endif]-->
+<?php }
+add_action( 'wp_head', 'ef_ie_html5_js' );
