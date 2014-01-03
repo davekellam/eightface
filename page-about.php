e87685439109c0c0082948e654de24d05513ed94
Dave Kellam <dave@eightface.com>
2014-01-03 03:56:09 -0500

    Adding page template for About page

---
+++ page-about.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+	<section id="primary">
+		
+		<?php if ( have_posts() ) : ?>
+			
+			<?php while ( have_posts() ) : ?> 
+
+				<?php the_post(); ?>
+
+				<?php the_content(); ?>
+		
+			<?php endwhile; ?>
+
+		<?php endif; ?>
+	
+	</section>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
