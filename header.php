58dd683082dff8cb946be563822eddbd31722bf4
Dave Kellam <dave@eightface.com>
2014-03-19 21:30:40 -0400

    Moving to async typekit

--- header.php
+++ header.php
@@ -23,8 +23,7 @@
 	<link rel="shortcut icon" href="/favicon.ico" />
 	<link rel="apple-touch-icon-precomposed" href="/images/eightface/logo.128.png"/>
 
-	<script type="text/javascript" src="http://use.typekit.net/yjc3syb.js"></script>
-	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+	<script type="text/javascript">(function(d){var config = {kitId: 'yjc3syb',scriptTimeout:3000},h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)})(document);</script>
 </head>
 
 <body <?php body_class(); ?>>
